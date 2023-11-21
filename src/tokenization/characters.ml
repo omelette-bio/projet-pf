593d85d97029915c90618aaf05e9cb1b909794ff
omelette-bio <omelettebio@proton.me>
2023-11-21 14:47:40 +0100

    modified:   src/tokenization/characters.ml

--- src/tokenization/characters.ml
+++ src/tokenization/characters.ml
@@ -6,15 +6,34 @@ module Tokenizer = struct
 
   exception EncodingError of string
 
+
+  let index l e = 
+    let rec index_aux l e i = match l with
+      | [] -> None
+      | hd::tl -> if hd = e then Some i 
+                  else (index_aux tl e (i+1))
+    in index_aux l e 0
+    
   let encode voc s =
-    ignore (voc, s);
-    failwith "todo"  
+    let len = String.length s in
+    let encoded = ref [] in
+    for i=0 to len-1 do
+      match index voc s.[i] with
+      | Some j -> encoded := !encoded @ [j]
+      (* raise an error with the remaining part of the string *)
+      | None -> raise (EncodingError (Printf.sprintf "%s" (String.sub s i (len-i))))
+    done;
+    !encoded
 
   exception DecodingError of int
 
   let decode voc ids =
-    ignore (voc, ids);
-    failwith "todo"  
+    let rec decode_aux voc ids decoded = match ids with
+      | [] -> decoded
+      | hd::tl -> match (List.nth_opt voc hd) with
+        | Some c -> decode_aux voc tl (decoded ^ (String.make 1 c))
+        | None -> raise (DecodingError hd)
+    in decode_aux voc ids ""
  
   let learn batch =
     ignore batch; 
