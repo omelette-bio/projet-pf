d96e96d425d9507d62301f5b0520cb6abb457990
omelette-bio <omelettebio@proton.me>
2023-12-11 13:54:45 +0100

    new file:   data/yoshikage.txt 	new file:   data/yoshikagefr.txt 	modified:   test/text_generator/text_generator.ml

--- test/text_generator/text_generator.ml
+++ test/text_generator/text_generator.ml
@@ -2,7 +2,7 @@ open MarkovChain
 
 let () = 
   TextGenerator.run  
-    ~files: ["../../../../data/small_swann.txt"] 
+    ~files: ["../../../../data/small_swann.txt";"../../../../data/yoshikagefr.txt"] 
     ~window_size: 5
-    ~output_length: 10000
+    ~output_length: 500
   |> Format.printf "%s@."
